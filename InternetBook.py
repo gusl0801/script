9a6204bdff7398359b3c9a7e83f9c6f2e632c5e6
gusl0801 <gusl0801@daum.net>
2017-05-21 04:02:26 +0900

    Connect to OpenAPI
    prev way : using HTTPConnection
    cur way : using request module

--- InternetBook.py
+++ InternetBook.py
@@ -1,7 +1,9 @@
 from XMLBook import *
 from http.client import HTTPConnection
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import requests
 
+MAX_RETRIES = 20
 
 conn = None
 regKey = '1ckRGPeUTj7n2EeO5dyg6aaV8FOMSVfUr%2FRc%2Bsp47rkQ8dqRTygAs3vZoJ%2BZ%2B%2BvkBJDqmHZh9lgOrNq%2FlEN6jQ%3D%3D'
@@ -10,8 +12,8 @@ server = 'openapi-lib.sen.go.kr'
 
 """
 'http://openapi-lib.sen.go.kr' \
-'/openapi/service/lib/openApi' \
-'?serviceKey=1ckRGPeUTj7n2EeO5dyg6aaV8FOMSVfUr%2FRc%2Bsp47rkQ8dqRTygAs3vZoJ%2BZ%2B%2BvkBJDqmHZh9lgOrNq%2FlEN6jQ%3D%3D
+'/openapi/service/lib/openApi
+?serviceKey=1ckRGPeUTj7n2EeO5dyg6aaV8FOMSVfUr%2FRc%2Bsp47rkQ8dqRTygAs3vZoJ%2BZ%2B%2BvkBJDqmHZh9lgOrNq%2FlEN6jQ%3D%3D
 &title=%EB%8F%84%EB%91%91' \
 '&manageCd=MB' \
 '&numOfRows=5' \
@@ -19,21 +21,23 @@ server = 'openapi-lib.sen.go.kr'
 '&pageNo=2' \
 '&startPage=2'
 """
+
+"""
+http://openapi-lib.sen.go.kr/openapi/service/lib/openApi
+?serviceKey=1ckRGPeUTj7n2EeO5dyg6aaV8FOMSVfUr%2FRc%2Bsp47rkQ8dqRTygAs3vZoJ%2BZ%2B%2BvkBJDqmHZh9lgOrNq%2FlEN6jQ%3D%3D
+&title=%EB%8F%84%EB%91%91
+&manageCd=MB
+&numOfRows=5
+&pageSize=5
+&pageNo=2
+&startPage=2
+"""
 def URIBuilder(server, **user):
-    str = "https://" + server + "/openapi/service/lib/openApi?"
+    str = "http://" + server + "/openapi/service/lib/openApi?"
 
     for key in user.keys():
         str += key + '=' + user[key] + '&'
-    return 'http://openapi-lib.sen.go.kr' \
-'/openapi/service/lib/openApi' \
-'?serviceKey=1ckRGPeUTj7n2EeO5dyg6aaV8FOMSVfUr%2FRc%2Bsp47rkQ8dqRTygAs3vZoJ%2BZ%2B%2BvkBJDqmHZh9lgOrNq%2FlEN6jQ%3D%3D\
-&title=%EB%8F%84%EB%91%91' \
-'&manageCd=MB' \
-'&numOfRows=5' \
-'&pageSize=5' \
-'&pageNo=2' \
-'&startPage=2'
-    # return str
+    return str
 
 def extractBookData(strXml):
     from xml.etree import ElementTree
@@ -46,36 +50,47 @@ def extractBookData(strXml):
         isbn = item.find("isbn")
         strTitle = item.find("title")
         print (strTitle)
+        print(strTitle.text)
         if len(strTitle.text) > 0 :
            return {"ISBN":isbn.text,"title":strTitle.text}
 
 def connectOpenAPIServer():
     global conn, server
     conn = HTTPConnection(server)
-    aa = 1
 
 def getBookDataFromTitle(title):
     global server, regKey, conn
     if conn == None:
         connectOpenAPIServer()
     # uri = userURIBuilder(server, key=regKey, query='%20', display="1", start="1", target="book_adv", d_isbn=isbn)
-    uri = URIBuilder(server, apikey=regKey, manageCd = "MB", numOfRows = '5', pageNo = '5')
-    conn.request("GET", 'http://openapi-lib.sen.go.kr' \
-'/openapi/service/lib/openApi' \
-'?serviceKey=1ckRGPeUTj7n2EeO5dyg6aaV8FOMSVfUr%2FRc%2Bsp47rkQ8dqRTygAs3vZoJ%2BZ%2B%2BvkBJDqmHZh9lgOrNq%2FlEN6jQ%3D%3D\
-&title=%EB%8F%84%EB%91%91' \
-'&manageCd=MB' \
-'&numOfRows=5' \
-'&pageSize=5' \
-'&pageNo=2' \
-'&startPage=2')
-
+    uri = URIBuilder(server, serviceKey =regKey,
+                     title = '%EB%8F%84%EB%91%91',
+                     manageCd = "MB",
+                     numOfRows = '5',
+                     pageNo = '5',
+                     startPage = '2')
+    # 기본 코드
+    """
+    conn.request("GET", uri)
     req = conn.getresponse()
     print(req.status)
-
-    if int(req.status) == 200:
+    
+    if int(req.status) == 200 :
         print("Book data downloading complete!")
         return extractBookData(req.read())
+    else:
+        print ("OpenAPI request has been failed!! please retry")
+        return None
+    """
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
+    req = requests.get(uri, headers=headers)
+
+    print(req.status_code)
+
+    if int(req.status_code) == 200:
+        print("Book data downloading complete!")
+        #return extractBookData(req.read())
+        return extractBookData(req.content)
     else:
         print("OpenAPI request has been failed!! please retry")
         return None
