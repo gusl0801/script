f3d60f0e838310b049c2af8dbfa050f78abe27af
gusl0801 <gusl0801@daum.net>
2017-06-03 20:33:03 +0900

    add a functon that sending a maill
    ..

--- mail.py
+++ mail.py
@@ -1,3 +1,4 @@
+"""
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -53,3 +54,42 @@ class MailHandler:
 
     def Send(self):
         pass
+    
+"""
+
+# -*- coding: cp949 -*-
+import mimetypes
+import smtplib
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+
+#global value
+host = "smtp.gmail.com" # Gmail STMP 서버 주소.
+port = "587"
+htmlFileName = "logo.html"
+
+senderAddr = "scoke0801@gmail.com"     # 보내는 사람 email 주소.
+recipientAddr = "scoke0801@daum.net"   # 받는 사람 email 주소.
+
+msg = MIMEBase("multipart", "alternative")
+msg['Subject'] = "Test email in Python 3.6"
+msg['From'] = senderAddr
+msg['To'] = recipientAddr
+
+# MIME 문서를 생성합니다.
+htmlFD = open(htmlFileName, 'rb')
+HtmlPart = MIMEText(htmlFD.read(),'html', _charset = 'UTF-8' )
+htmlFD.close()
+
+# 만들었던 mime을 MIMEBase에 첨부 시킨다.
+msg.attach(HtmlPart)
+
+# 메일을 발송한다.
+s = smtplib.SMTP(host,port)
+#s.set_debuglevel(1)        # 디버깅이 필요할 경우 주석을 푼다.
+s.ehlo()
+s.starttls()
+s.ehlo()
+s.login("scoke0801@gmail.com","dla753156")
+s.sendmail(senderAddr , [recipientAddr], msg.as_string())
+s.close()
